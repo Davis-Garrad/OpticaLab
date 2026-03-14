ec744c7a8c00e9f1b3bcf1bc489df65142b3186b
Davis-Garrad <Davis.Garrad@gmail.com>
2026-03-13 23:54:08 -0400

    Fixed annoying space in test_suite, updated todo list for later

--- test_suite.py
+++ test_suite.py
@@ -1,23 +1,23 @@
 import sys
 
-if(input('scene setup? [y/N]') == 'y'):
+if(input('scene setup? [y/N] ') == 'y'):
     sys.argv = ['virtual','tests/cfg/test_cfg_flags_00.txt']
     import tests.scene_setup
-if(input('test_00 (Free ray)? [y/N]') == 'y'):
+if(input('test_00 (Free ray)? [y/N] ') == 'y'):
     sys.argv = ['virtual','tests/cfg/test_cfg_flags_00.txt']
     import tests.test_00
-if(input('test_01 (Basic Snell\'s law test)? [y/N]') == 'y'):
+if(input('test_01 (Basic Snell\'s law test)? [y/N] ') == 'y'):
     sys.argv = ['virtual','tests/cfg/test_cfg_flags_00.txt']
     import tests.test_01
-if(input('test_02 (Rotations+scaling)? [y/N]') == 'y'):
+if(input('test_02 (Rotations+scaling)? [y/N] ') == 'y'):
     sys.argv = ['virtual','tests/cfg/test_cfg_flags_00.txt']
     import tests.test_02
-if(input('test_03 (Spherical lens, thick)? [y/N]') == 'y'):
+if(input('test_03 (Spherical lens, thick)? [y/N] ') == 'y'):
     sys.argv = ['virtual','tests/cfg/test_cfg_flags_00.txt']
     import tests.test_03
-if(input('test_04 (Spherical lens, timing)? [y/N]') == 'y'):
+if(input('test_04 (Spherical lens, timing)? [y/N] ') == 'y'):
     sys.argv = ['virtual','tests/cfg/test_cfg_flags_04.txt']
     import tests.test_04
-if(input('test_05 (Mirror)? [y/N]') == 'y'):
+if(input('test_05 (Mirror)? [y/N] ') == 'y'):
     sys.argv = ['virtual','tests/cfg/test_cfg_flags_00.txt']
     import tests.test_05
