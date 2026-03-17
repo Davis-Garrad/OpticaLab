21c29ba62e09de9f66f808cf6201ccc65af36204
Davis-Garrad <Davis.Garrad@gmail.com>
2026-03-17 13:38:31 -0400

    Actually implemented the explicit frontfacing normal for PlanoConvex...

--- parts_lib.py
+++ parts_lib.py
@@ -7,8 +7,8 @@ class LensPlanoConvex(scene.SceneObject):
         front_profile = lambda x,y: np.sqrt(np.square(radius)-np.square(x)) - np.sqrt(np.square(radius) - 1)
         
         def ffn(x,y):
-            diff_from_centre = np.array([x,y,front_profile(x,y)])# - np.array([0,0,-np.sqrt(np.square(radius)-1)])
-            return diff_from_centre# / np.sqrt(np.sum(np.square(diff_from_centre)))
+            diff_from_centre = np.array([x,y,front_profile(x,y)]) - np.array([0,0,-np.sqrt(np.square(radius)-1)])
+            return diff_from_centre# / np.sqrt(np.sum(np.square(diff_from_centre))) # doesn't need to be normalized! Will do later.
 
         super().__init__(index_of_refraction, front_profile, position, rotation, scale, frontfacing_normal=ffn)
 
